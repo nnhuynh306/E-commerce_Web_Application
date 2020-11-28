9043c683e223b921d970267974ce382419aef992
Nam Huynh Nhat <nnhuynh306@gmail.com>
2020-11-28 22:57:22 +0700

    deploy server

---
+++ main.js
@@ -0,0 +1,19 @@
+const PORT = process.dnv.PORT || 5000;
+const express = require(`express`);
+const {get} = require('http');
+const fs = require('fs');
+
+var app = express();
+
+app.use(express.static('src'));
+
+app.get('/', (req, res) => {
+    fs.readFile('src/UI/homepage1.html', (err, data) => {
+        res.header(200, { 'Content-Type': 'text/html'})
+        res.send(data);
+    })
+})
+
+app.listen(PORT, ()=> {
+    console.log('Server running');
+})
