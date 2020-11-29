8610aa2028d56c4b323badc94b45930fb4614d18
Nam Huynh Nhat <nnhuynh306@gmail.com>
2020-11-29 08:59:05 +0700

    commit

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-const PORT = process.env.PORT || 5000;
-const express = require(`express`);
-const {get} = require('http');
-const fs = require('fs');
+const express = require('express')
+const fs = require('fs')
+const { get } = require('http')
 
-var app = express();
+const app = express()
+const port = 3000
 
-app.use(express.static('src'));
+app.use(express.static('src'))
 
-app.get('/', (req, res) => {
-    fs.readFile('src/UI/homepage1.html', (err, data) => {
-        res.header(200, { 'Content-Type': 'text/html'})
-        res.send(data);
-    })
-})
+// app.get('/', (req, res) => {
+//   fs.readFile('/src/homepage1.html', (err, data) => {
+//       console.log(data);
+//       res.set('Content-Type', 'text/html');
+//       res.send(data);
+//   })
 
-app.listen(PORT, ()=> {
-    console.log('Server running');
+// })
+
+app.get('/', function(req, res) {
+    res.sendFile('/src/UI/login.html', {root: __dirname })
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
