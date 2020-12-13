73f5175646a7287752d6c103c7fe969c5f085873
maidangkhanh <mai.d.khanh@gmail.com>
2020-12-13 10:50:01 +0700

    Remove points from product detail

--- src/routes/product-detailRouter.js
+++ src/routes/product-detailRouter.js
@@ -2,6 +2,7 @@ const express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res) {
+    console.log(req.query.id || 1);
     res.render('product-detail')
 });
 
