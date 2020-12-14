2f6ec0e7d60d2cb1050d780c45d67c357e30ed49
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2020-12-14 10:33:45 +0700

    Fix đổ data lên Product Detail

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,26 +3,17 @@ const controller = {}
 const models = require('../models');
 const Op = require('sequelize').Op;
 
-controller.getAll = (userID, productID) => {
-    models.Comment.findAll({
+controller.getProductComment = (productID) => {
+    return models.Comment.findAll({
         where: {
-            [Op.and]: [
-                {
-                    UserId: {
-                        [Op.eq]: userID
-                    }
-                },
-                {
-                    ProductId: {
-                        [Op.eq]: productID
-                    }
-                },
-            ]
+            ProductId: productID
         },
-    }).then(data => {
-        return data.Comment
-    })
-}
+        include: [{
+            model: models.User,
+            attributes:['name']
+       }]
+    });
+};
 
 
 
--- src/routes/product-detailRouter.js
+++ src/routes/product-detailRouter.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const controller = require('./../controllers/productController')
+const commentController = require('./../controllers/commentController');
+const { response } = require('express');
 var router = express.Router()
 
 router.get('/', function(req, res) {
     var productID = req.query.id || 1;
-    res.locals.product = controller.getProductById(productID);
+
+    
     // res.json() = controller.getProductById(productID);
-    res.render('product-detail');
+    commentController.getProductComment(productID).then(cmt => {
+        res.locals.comment = cmt
+        console.log(cmt)
+        controller.getProductById(productID).then(product => {
+            res.locals.product = product
+            res.render('product-detail');
+        })
+
+    });
+
 });
 
 module.exports = router
