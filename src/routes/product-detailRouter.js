2ac0aa4fc5eca79463a312a71b80a863cac50799
maidangkhanh <mai.d.khanh@gmail.com>
2020-12-17 16:50:16 +0700

    Change [roduct detail routing
    Change from query string to req.params

--- src/routes/product-detailRouter.js
+++ src/routes/product-detailRouter.js
@@ -5,9 +5,36 @@ const { response } = require('express');
 const toColor = require('../utility/colorConverter');
 var router = express.Router()
 
-router.get('/', function(req, res) {
+// router.get('/', function(req, res) {
+//     const comment=[]
+//     var productID = req.query.id || 1;
+//     commentController.getProductComment(productID).then(cmt => {
+//         cmt.forEach(element => {
+//             comment.push(element.dataValues)
+//         });
+//         comment.forEach(element=>{
+//             let tmp=[]
+//             tmp=JSON.stringify(element.createdAt)
+//             tmp=tmp.substr(1,10)
+//             element.createdAt=tmp
+//         })
+//         res.locals.comment = cmt
+//         controller.getProductById(productID).then(product => {
+//             colors = [];
+//             res.locals.product = product;
+//             for(attr of product.ProductAttributes)
+//             {
+//                 colors.push({color:toColor(attr.name)});
+//             }
+//             res.locals.colors = colors;
+//             res.render('product-detail');
+//         })
+//     });
+// });
+
+router.get('/:id', function(req, res) {
     const comment=[]
-    var productID = req.query.id || 1;
+    var productID = req.params.id;
     commentController.getProductComment(productID).then(cmt => {
         cmt.forEach(element => {
             comment.push(element.dataValues)
@@ -29,9 +56,7 @@ router.get('/', function(req, res) {
             res.locals.colors = colors;
             res.render('product-detail');
         })
-
     });
-
 });
 
 module.exports = router
