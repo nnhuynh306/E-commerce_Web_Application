ad83a43b7a6bca5618911b9e88d242e5df24db72
maidangkhanh <mai.d.khanh@gmail.com>
2020-12-17 17:16:57 +0700

    Fix routing shop-fullwidth

--- src/routes/product-detailRouter.js
+++ src/routes/product-detailRouter.js
@@ -5,36 +5,9 @@ const { response } = require('express');
 const toColor = require('../utility/colorConverter');
 var router = express.Router()
 
-// router.get('/', function(req, res) {
-//     const comment=[]
-//     var productID = req.query.id || 1;
-//     commentController.getProductComment(productID).then(cmt => {
-//         cmt.forEach(element => {
-//             comment.push(element.dataValues)
-//         });
-//         comment.forEach(element=>{
-//             let tmp=[]
-//             tmp=JSON.stringify(element.createdAt)
-//             tmp=tmp.substr(1,10)
-//             element.createdAt=tmp
-//         })
-//         res.locals.comment = cmt
-//         controller.getProductById(productID).then(product => {
-//             colors = [];
-//             res.locals.product = product;
-//             for(attr of product.ProductAttributes)
-//             {
-//                 colors.push({color:toColor(attr.name)});
-//             }
-//             res.locals.colors = colors;
-//             res.render('product-detail');
-//         })
-//     });
-// });
-
 router.get('/:id', function(req, res) {
     const comment=[]
-    var productID = req.params.id;
+    var productID = req.params.id || 1;
     commentController.getProductComment(productID).then(cmt => {
         cmt.forEach(element => {
             comment.push(element.dataValues)
