098663211c2c8445e26dbbe374475dde92ca247c
HuyHoang <huyhoanghonguyen20@gmail.com>
2020-12-18 17:28:08 +0700

    admin edit-product content tab

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -3,9 +3,9 @@ var router = express.Router()
 let userController = require('../controllers/userController')
 
 router.get('/', function (req, res) {
-    res.render("login", {
+    res.render("admin", {
+        pageStyle: "admin-controller"
     })
-
 });
 
 module.exports = router
