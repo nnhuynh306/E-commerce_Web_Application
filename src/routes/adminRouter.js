c72e9929217e0aead25c7196f1fdcd5dd344d96c
HuyHoang <huyhoanghonguyen20@gmail.com>
2020-12-19 13:44:15 +0700

    edit-product result hbs tested

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -4,7 +4,15 @@ let userController = require('../controllers/userController')
 
 router.get('/', function (req, res) {
     res.render("admin", {
-        pageStyle: "admin-controller"
+        pageStyle: "admin-controller",
+        product_find_result: {
+            product_name_result: "Nước rửa tay con mèo vằn",
+            stock_result: 69,
+            price_result: 99.8,
+            category_result: "Nước hoa",
+            description_result: "Hương thơm ngào ngạt toát ra làm tôi thấy vui vẻ"
+        },
+
     })
 });
 
